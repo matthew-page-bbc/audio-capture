b5f4f06be7ac492f811b795bd299da0f330dd586
Matthew Page <matthew@aslivemedia.com>
2022-08-15 16:48:11 +0100

    updated test case

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 'use strict';
 const {AudioCapture, LevelMeter} = require('./index.js');
 
-var meter = new LevelMeter();
+var meter = new LevelMeter({
+    wordLength: 16
+});
 meter.on('level', (data) => {
     console.log(data);
 });
 
 var capture = new AudioCapture({
-    device: "default:CARD=AudioPCI",
+    device: "plughw:1",
     channels: 8,
-    sampleRate: 48000
+    sampleRate: 48000,
+    wordLength: 16
 });
 
 capture.on('sample', (data) => {
